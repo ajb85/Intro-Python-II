e468601128a4beb108ea23225bc91193b4c054bd
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-29 18:47:21 -0400

    Creates player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from id import setID
+getID = setID()
+
+class Player:
+    def __init__(self, name, location="r1", inventory=[]):
+        self.id = f"p{getID()}"
+        self.name = name
+        self.location = location
+        self.inventory = inventory
+    def pickUpItem(id):
+        self.inventory.append(id)
+    def dropItem(id):
+        self.inventory.remove(id)
+    def moveToRoom(id):
+        self.location = id
