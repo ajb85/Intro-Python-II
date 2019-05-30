61a2df773ce4a30aa7225253d74ec20baf8caf0c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-30 13:12:50 -0400

    Fixes bugs with game running.  Now runs without bugs and allows 'q' to exit any time

--- src/adv.py
+++ src/adv.py
@@ -3,21 +3,52 @@ from player import Player
 from item import Item
 from roomInfo import roomInfo
 
-# Declare all the rooms
 
 class Game:
-    def __init__(self, roomsInfo, items=[]):
-        self.rooms = self._getRooms(roomsInfo)
+    def __init__(self, roomsInfo, items=None):
+        self.rooms = self._createRooms(roomsInfo)
         self.items = items
 
-    def _findByID(self, collection, id):
-        for thing in self[collection]:
-            if thing.id is id:
-                return thing
+    def start(self):
+        while True:
+            print("Welcome to the game!  As of right now, you can walk around rooms but that's unfortunately it!\nEnter 'q' at any prompt to quit any time!")
+            playerName = self._getPlayerName()
+            if playerName == "q":
+                break
+            else:
+                self.player = Player(playerName)
+                self._enterRoom(self.player.location)
+                break
+                
+    def _findRoomByID(self, id):
+        for key in self.rooms:
+            if self.rooms[key].id == id:
+                return self.rooms[key]
+
+
+    def _selectRoom(self, room):
+        while True:
+            direction = input(f"\nWhich direction would you like to go?  There are doorways in these directions:\n{', '.join(room.connections.keys())}: ")
+            if direction == "q":
+                break
+            elif direction in room.connections:
+                self._enterRoom(room.connections[direction])
+                break
+
+    def _enterRoom(self, roomID):
+        self.player.location = roomID
+        room = self._findRoomByID(roomID)
+        room.enter(self.player.name)
+        self._selectRoom(room)
+
+    def _getPlayerName(self):
+        playerName = input("\nPlease enter a name for your character: ")
+        if len(playerName) > 0:
+            return playerName
 
     def _linkRooms(self, rooms):
         for key in rooms:
-            # Build links for each room.  rooms[key] is the class object for a room.
+            # Builds links for each room.  rooms[key] is the class object for a room.
             # The links are created from the provided links in roomInfo.
             # However, the connecting room's id must be found to create the connection
             links = roomInfo[key]["links"]
@@ -26,7 +57,7 @@ class Game:
                 id = rooms[linkedKey].id
                 rooms[key].connectRooms(id, direction)
 
-    def _getRooms(self, roomsInfo):
+    def _createRooms(self, roomsInfo):
         rooms = {}
         for key in roomInfo:
             # Iterate roomInfo and for each 'key', create a new Room class, populate 'rooms'
@@ -35,56 +66,8 @@ class Game:
         # _linkRooms will mutate the objects, no need to assign
         self._linkRooms(rooms)
         return rooms
-
-    def commands(self, input):
-        commands = {"q":False}
-        if input in commands:
-            return commands[input]
-        else:
-            return None
     
-    def _getName(self):
-        while True:
-            input = Input("Please enter a name for your character: ")
-            command = 
-            if len(playerName) > 0:
-                return playerName
-
-    def start(self):
-        while True:
-            playerName = self._getName()
-            self.player = Player(playerName)
-
-
-    def exploreRoom(self, id):
-        room = self._findByID("rooms", id)
-        print("You look around the room, here's what you see: ")
-        for item in room.items:
-            print(item)
-    
-    def enterRoom(self, id):
-        self.player.moveToRoom(id)
-        room = self._findByID("rooms", id)
-        print(room.description)
-
 
     
 game = Game(roomInfo)
-
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:``
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
-
+game.start()
