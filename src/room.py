96b70d108d7e1ebf1d816d75c889b016d46fac29
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-05-30 13:13:10 -0400

    Adds 'enter' method and clears the console each time the user enters a room

--- src/room.py
+++ src/room.py
@@ -1,6 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+import os
+
 from id import setID
 getID = setID()
 
@@ -14,16 +16,15 @@ class Room:
         self.poi = poi
     def updateAttrs(self, attr, update):
         self[attr] = update
-    def pickUpItem(itemName):
+    def pickUpItem(self, temName):
         index = 0
         for i in range(0, len(items)):
             if(items[i].name is itemName):
                 index = i
         return items.pop(index)
-    def enter(self):
-        print(self.description)
-    def describe(self, str):
-        self.description = str
+    def enter(self, playerName):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"\n{playerName} moves to the {self.name}.\n{self.description}")
     def connectRooms(self, roomID, toDirection):
         if self.connections is None:
             self.connections = {toDirection: roomID}
